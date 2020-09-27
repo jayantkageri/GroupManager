ef106ed5f41fb4003d508cbb7efc01d5deaceaba
ᴊᴀʏᴀиᴛ ʜᴇɢᴅᴇ ᴋᴀɢᴇяɪ <69081616+jayantkageri@users.noreply.github.com>
2020-09-27 11:21:19 +0530

    Update __main__.py

--- haruka/__main__.py
+++ haruka/__main__.py
@@ -29,7 +29,7 @@ I'm group manager bot and Anti-spam for RR Players
 Activate Anti-spam protection in your BM by this command : 
 /antispam on 
 
-This Bo is Made By [Jayant](t.me/jayantkageri)
+This Bot is Made By [Jayant](t.me/jayantkageri) from the Source of Haruka Aya and Marie
 
 Click /help or Help button below to find out more about how to use me to my full potential.
 
